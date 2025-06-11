b96d2e57acb89e5ecee6623450cda8610fa0f505
Davron Abduvahobov <davronabduvahobov@gmail.com>
2025-06-11 15:32:58 +0500

    Update

--- Day2/todo2.py
+++ Day2/todo2.py
@@ -1,4 +1,4 @@
 todos = ["Work Hard and Buy BMW", "Get ticket to fly Dubai"]
 goal = len(todos)
 message = "You have " + str(goal) + " goals now! "
-print(todos, message)
+print(todos, "\n", message)
